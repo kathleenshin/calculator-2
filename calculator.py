b6e7152f589c749bde43ea61d811c52ecad090e7
vagrant <vagrant@vagrant.vm>
2021-03-17 04:07:34 +0000

    Completed pseudocode

--- calculator.py
+++ calculator.py
@@ -4,4 +4,25 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
                         power, mod, )
 
 
-# Replace this with your code
+# tokenize on spaces
+
+# use split string method to create the list
+# evaluate the tokens -- first item in list
+# if first item in new list == "add"
+#   run add function 
+# if first item in new list == "subtract"
+#   run subtract function
+# if first item in new list == "multiply"
+#   run multiply function
+# if first item in new list == "divide"
+#   run divide function
+#if first item in new list == "square"
+#   run square function
+#if first item in new list == "cube"
+#   run cube function
+#if first item in new list == "power"
+#   run power fuction
+#if first item in new list == "mod"
+#   run mod function
+#if first item in new list == "q"
+#   exit calculator program loop
